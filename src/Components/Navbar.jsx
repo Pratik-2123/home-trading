c4c75c9f257358901196bde450a607fb58526c18
Pratik-2123 <pratikpatel7057347707@gmail.com>
2024-07-20 19:33:36 +0530

    added authentication functionality

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import tradingLogo from '../assets/increase.png'; // Update the path to your logo
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -19,17 +23,40 @@ const Navbar = () => {
       <div className='hidden md:flex'>
         <ul className='flex text-white gap-20 text-lg pr-10 cursor-pointer'>
           <Link to="home" spy={true} offset={-100} duration={500} smooth={true}>
-            <li className='hover:text-[#7db6f6]'>Home</li>
+            <li className='hover:text-[#7db6f6] pt-1'>Home</li>
           </Link>
           <Link to="product" spy={true} offset={-100} duration={500} smooth={true}>
-            <li className='hover:text-[#7db6f6]'>Product</li>
+            <li className='hover:text-[#7db6f6] pt-1'>Product</li>
           </Link>
           <Link to="help" spy={true} offset={-100} duration={500} smooth={true}>
-            <li className='hover:text-[#7db6f6]'>Help</li>
+            <li className='hover:text-[#7db6f6] pt-1'>Help</li>
           </Link>
           <Link to="contact" spy={true} offset={-7} duration={500} smooth={true}>
-            <li className='hover:text-[#7db6f6]'>Contact</li>
+            <li className='hover:text-[#7db6f6] pt-1'>Contact</li>
           </Link>
+
+          {
+            isAuthenticated ? <li>
+              <button
+                className="bg-[#7747FF] border-2 border-[#9b9b9b00] rounded-lg text-white px-4 py-1.5 text-base hover:border-[#fff] cursor-pointer transition"
+                onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+              >
+                Log out
+              </button>
+            </li>
+              :
+              <li>
+                <button
+                  className="bg-[#7747FF] border-2 border-[#9b9b9b00] rounded-lg text-white px-4 py-1.5 text-base hover:border-[#fff] cursor-pointer transition"
+                  onClick={() => loginWithRedirect()}
+                >
+                  Log in
+                </button>
+              </li>
+          }
+
+
+
         </ul>
       </div>
       <div className='md:hidden flex items-center pr-10'>
@@ -52,6 +79,25 @@ const Navbar = () => {
             <Link onClick={toggleMenu} to="contact" spy={true} offset={0} duration={500} smooth={true}>
               <li className='hover:text-[#7db6f6]'>Contact</li>
             </Link>
+            {
+              isAuthenticated ? <li>
+                <button
+                  className="bg-[#7747FF] border-2 border-[#9b9b9b00] rounded-lg text-white px-4 py-1.5 text-base hover:border-[#fff] cursor-pointer transition"
+                  onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+                >
+                  Log out
+                </button>
+              </li>
+                :
+                <li>
+                  <button
+                    className="bg-[#7747FF] border-2 border-[#9b9b9b00] rounded-lg text-white px-4 py-1.5 text-base hover:border-[#fff] cursor-pointer transition"
+                    onClick={() => loginWithRedirect()}
+                  >
+                    Log in
+                  </button>
+                </li>
+            }
           </ul>
         </div>
       )}
